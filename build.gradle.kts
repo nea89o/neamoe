176e37844c17eaabb5c9da0f9c23237df9c0b5a8
nea <romangraef@gmail.com>
2022-08-26 14:56:23 +0200

    idk make it maveny

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,47 @@
 plugins {
-    kotlin("js") version "1.7.10"
+    kotlin("multiplatform") version "1.7.10"
+    `maven-publish`
 }
 
-allprojects {
-    apply(plugin = "org.jetbrains.kotlin.js")
-    repositories {
-        mavenCentral()
-    }
-
-    kotlin {
-        sourceSets.all {
-            languageSettings.optIn("kotlin.ExperimentalStdlibApi")
-            languageSettings.optIn("kotlin.RequiresOptIn")
-        }
-        js(IR) {
-            browser {
-                commonWebpackConfig {
-                    sourceMaps = true
-                    cssSupport.enabled = true
-                }
-            }
-            binaries.executable()
-        }
-    }
-
-    afterEvaluate {
-        rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-            versions.webpackCli.version = "4.10.0"
-        }
-    }
-
-    dependencies {
-        implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.376"))
-        api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-        api("org.jetbrains.kotlin-wrappers:kotlin-styled")
-     //   implementation(npm("prop-types", "^15.6.2"))
-    }
+repositories {
+    mavenCentral()
 }
 group = "moe.nea"
 version = "0.0.1"
+
+kotlin {
+    sourceSets.all {
+        languageSettings.optIn("kotlin.ExperimentalStdlibApi")
+        languageSettings.optIn("kotlin.RequiresOptIn")
+    }
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                sourceMaps = true
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        named("jsMain") {
+            dependencies {
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                api("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.5-pre.376")
+            }
+        }
+    }
+}
+
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("library") {
+            from(components["kotlin"])
+        }
+    }
+}
+
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,7 +1,29 @@
+plugins {
+    kotlin("js")
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings.optIn("kotlin.ExperimentalStdlibApi")
+        languageSettings.optIn("kotlin.RequiresOptIn")
+    }
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                sourceMaps = true
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+}
+
 
 dependencies {
     implementation(npm("@fontsource/comic-mono", "^4.5.0"))
     implementation(rootProject)
 }
-
-
